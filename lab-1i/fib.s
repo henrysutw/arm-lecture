2352622a064587ae811ac60d0787d370405bbfa4
henrysutw <henrysutw@hotmail.com>
2015-03-14 00:53:00 +0800

    week2 lab-1i finished

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -13,31 +13,23 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	mov r5, #0
+	mov r4, #1
+	sub r3, r5, #1
+	
+	for_loop:
+		add r5, r4, r3
+		mov r3, r4
+		mov r4, r5
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+		subs r0, r0, #1
+		bge for_loop
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
+	mov r0, r4
 
 	pop {r3, r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
